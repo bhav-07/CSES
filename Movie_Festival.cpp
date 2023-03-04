a46c9bac052a4043b6b94d54425c23653ba18f42
bhav-07 <jangirbhavani201@gmail.com>
2023-03-04 15:43:47 +0530

    CSES problem set solution

---
+++ Movie_Festival.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define rep(i,a,b) for (int i = a;i<b;i++)
+#define repn(n) for (int i = 0;i<n;i++)
+#define repnj(n) rep(j,0,n)
+#define all(x) (x).begin(),(x).end()
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int,int>
+#define ui unordered_set<int>
+#define us unordered_set<string>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+const int MOD = 1e9+7;
+const int MOD1 = 998244353;
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n=1;
+    cin>>n;
+    
+    return 0;
+}
