bf8c5e69bc060965fd7da66e1b5eb05692f4822a
bhav-07 <jangirbhavani201@gmail.com>
2023-03-05 11:43:23 +0530

    CSES problem set

---
+++ Bit_Strings.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define rep(i,a,b) for (int i = a;i<b;i++)
+#define repn(n) for (int i = 0;i<n;i++)
+#define repnj(n) rep(j,0,n)
+#define all(x) (x).begin(),(x).end()
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int,int>
+#define ui unordered_set<int>
+#define us unordered_set<string>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+const int MOD = 1e9+7;
+const int MOD1 = 998244353;
+int exp(int x, unsigned int y, int p){
+
+    //modular exponentiation
+
+	int res=1; x=x%p;
+	while(y>0){
+		if (y&1) res= (res*x)%p; y=y>>1; x=(x*x)%p;
+	}
+	return res;
+}
+signed main(){
+    int n; 
+    cin>>n;
+	cout<<exp(2,n,MOD);
+    return 0;
+}
