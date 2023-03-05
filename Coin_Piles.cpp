bd7eb1b0e390584c42eb8d91d049fa2648725458
bhav-07 <jangirbhavani201@gmail.com>
2023-03-05 12:21:22 +0530

    CSES problem set solution

---
+++ Coin_Piles.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define rep(i,a,b) for (int i = a;i<b;i++)
+#define repn(n) for (int i = 0;i<n;i++)
+#define repnj(n) rep(j,0,n)
+#define all(x) (x).begin(),(x).end()
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int,int>
+#define ui unordered_set<int>
+#define us unordered_set<string>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+const int MOD = 1e9+7;
+const int MOD1 = 998244353;
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--)
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<((a+b)%3==0&& 2*a>=b && 2*b>=a ? "YES":"NO")<<endl;
+}
+    return 0;
+}
