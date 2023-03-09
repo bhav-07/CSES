1e04a69740a67134759470bd060fc2bc6e352b9c
bhav-07 <jangirbhavani201@gmail.com>
2023-03-09 21:22:04 +0530

    CSES problem set solutions

--- Apartments.cpp
+++ Apartments.cpp
@@ -1,45 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define int long long
-#define rep(i,a,b) for (int i = a;i<b;i++)
-#define repn(n) for (int i = 0;i<n;i++)
-#define repnj(n) rep(j,0,n)
-#define all(x) (x).begin(),(x).end()
-#define vi vector<int>
-#define vs vector<string>
-#define pii pair<int,int>
-#define ui unordered_set<int>
-#define us unordered_set<string>
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define endl "\n"
-const int MOD = 1e9+7;
-const int MOD1 = 998244353;
+
 const int MAX_N = 2e5;
 int n, m, k, a[MAX_N], b[MAX_N], ans;
 
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-    cin>>n>>m>>k;
-    repn(n) cin>>a[i];
-    repn(m) cin>>b[i];
-    sort(a,a+n);
-    sort(b,b+m);
-    int i=0,j=0;
-    while (i<n && j<n){
-        if(abs(a[i]-b[j]<=k)){
-            i++,j++,ans++;
-        }
-        else{
-            if(a[i]-b[j]>k) j++;
-            else i++;
-        }
-    }
-    cout<<ans<<endl;
+void solve() {
+	cin >> n >> m >> k;
+	for (int i = 0; i < n; ++i) cin >> a[i];
+	for (int i = 0; i < m; ++i) cin >> b[i];
+	sort(a, a + n);
+	sort(b, b + m);
+	int i = 0, j = 0;
+	while (i < n && j < m) {
+		if (abs(a[i] - b[j]) <= k) {
+			++i;
+			++j;
+			++ans;
+		} else {
+			if (a[i] - b[j] > k) {
+				++j;
+			}
+			else {
+				++i;
+			}
+		}
+	}
+	cout << ans << "\n";
+}
 
-    
-    return 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	solve();
+	return 0;
 }
